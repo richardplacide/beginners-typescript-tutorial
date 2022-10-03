44cc74e371aa6fa9ad25c12e9d5617fd7f810c63
Richard Placide <richard.devel@outlook.com>
2022-10-03 16:20:51 -0400

    exos done up to 06

--- src/02-object-param.problem.ts
+++ src/02-object-param.problem.ts
@@ -1,6 +1,11 @@
 import { expect, it } from "vitest";
 
-export const addTwoNumbers = (params) => {
+type paramsType = {
+  first: number,
+  second: number
+}
+
+export const addTwoNumbers = (params: paramsType) => {
   return params.first + params.second;
 };
 
